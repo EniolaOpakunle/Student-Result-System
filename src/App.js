15d2a303720b07facacea765376283ff37481be8
excellence-arch <oladipupomichael9@gmail.com>
2024-03-08 07:21:18 +0100

    edit student and logout working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import './App.css';
+import "./App.css";
 // import { BrowserRouter as Routes, Route} from 'react-router-dom';
 // import TeacherLogin from './TeacherLogin';
-import { Routes, Route } from 'react-router-dom';
-import AdminLogin from './components/AdminLogin';
-import AddStudent from './components/AddStudent';
-import StudentLogin from './components/StudentLogin';
-import Result from './components/Result';
-import AdminDashboard from './components/AdminDashboard';
+import { Routes, Route } from "react-router-dom";
+import AdminLogin from "./components/AdminLogin";
+import AddStudent from "./components/AddStudent";
+import StudentLogin from "./components/StudentLogin";
+import Result from "./components/Result";
+import AdminDashboard from "./components/AdminDashboard";
+import EditStudent from "./components/EditStudent";
 // import TeacherDashboard from './TeacherDashboard';
 
 const App = () => {
   return (
     <>
-    <Routes>
-      <Route exact path='/' element = {<StudentLogin/>}/>
-      <Route path='/result' element = {<Result/>}/>
-      <Route exact path="/adminlogin" element ={<AdminLogin/>} />
-      <Route path='/admindashboard' element={<AdminDashboard/>}/>
-      <Route path='/add-student' element={<AddStudent/>}/>
-    </Routes>
+      <Routes>
+        <Route exact path="/" element={<StudentLogin />} />
+        <Route path="/result" element={<Result />} />
+        <Route exact path="/adminlogin" element={<AdminLogin />} />
+        <Route path="/admindashboard" element={<AdminDashboard />} />
+        <Route path="/add-student" element={<AddStudent />} />
+        <Route path="/edit-student/:id" element={<EditStudent />} />
+      </Routes>
     </>
   );
 };
